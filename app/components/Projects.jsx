445604848398a80bffd0408be881a510a42a3425
Ugochukwu Emmanuel <ugohemmy@gmail.com>
2026-02-24 18:37:40 +0100

    still project adjustment

--- app/components/Projects.jsx
+++ app/components/Projects.jsx
@@ -12,13 +12,13 @@ const Projects = ({ Heading, Paragraph, Tech, Images, Button, Button1 }) => {
       <div className="md:relative p-2 bg-white/10 rounded-3xl">
       <Image className="rounded-3xl" src={Images} alt="Project Image" width={600} height={300} />
 
-      <div className="flex gap-4 justify-between item-center text-center md:absolute inset-0 md:opacity-0 md:hover:opacity-100 transition-opacity duration-300 mx-3 px-2">
-      <Link href={Button} className="bg-white/60 text-black px-4 py-2 rounded-lg h-10 translate-y-25 md:translate-y-30 hover:bg-amber-600 hover:text-white transition-all duration-300 ease-in-out" target="_blank">Live Demo</Link>
+      <div className="flex gap-2 justify-between item-center text-center md:absolute inset-0 md:opacity-0 md:hover:opacity-100 transition-opacity duration-300 px-2">
+      <Link href={Button} className="bg-white/60 text-black px-2 py-2 rounded-lg md:h-10 translate-y-25 md:mx-2 md:translate-y-30 hover:bg-amber-600 hover:text-white transition-all duration-300 ease-in-out" target="_blank">Live Demo</Link>
 
-      <Link href={Button1} className="bg-white/60 text-black px-4 py-2 rounded-lg h-10 translate-y-25 md:translate-y-30 hover:bg-amber-600 hover:text-white transition-all duration-300 ease-in-out" target="_blank">GitHub Repository</Link>
+      <Link href={Button1} className="bg-white/60 text-black px-2 py-2 rounded-lg md:h-10 translate-y-25 md:mx-2 md:translate-y-30 hover:bg-amber-600 hover:text-white transition-all duration-300 ease-in-out" target="_blank">GitHub Repository</Link>
       </div>
 
-      <div className="flex gap-6 text-center justify-center item-center md:pt-2">
+      <div className="flex gap-6 my-0 text-center justify-center item-center md:pt-2">
         {Tech.map((text, i) => (<p className="p-1.5 bg-black text-white/50 rounded-xl text-center text-[0.6rem]" key={i}>{text}</p>))}
       </div>
       </div>
